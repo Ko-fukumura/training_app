a7ce53db1f1fc486eab83cbd04e422f02d3a1657
福村晃 <fukumurako@fukumuraakiranoMacBook-puro.local>
2020-01-30 18:40:35 +0900

    パスワードカラム名の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,17 +14,24 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to root_url
+    if @user.save
+      # flash[:success]
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   def edit
-    
   end
 
   def update
-    @user = User.update(user_params)
-    redirect_to user_url(@user)
+    if @user = User.update(user_params)
+      # flash[:success]
+      redirect_to user_url(@user)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_083525) do
+ActiveRecord::Schema.define(version: 2020_01_30_093339) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.string "password", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
