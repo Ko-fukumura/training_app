797052a16f899d7ace558f4238930b4aaf5b564b
福村晃 <fukumurako@fukumuraakiranoMacBook-puro.local>
2020-02-11 04:43:50 +0900

    本番環境のデータベースを変更master13

--- app/mailers/sendgrid_mailer.rb
+++ app/mailers/sendgrid_mailer.rb
@@ -1,5 +1,5 @@
 class SendgridMailer < ApplicationMailer
   def test_mail
-    mail(from: 'from@example.com', to: 'shamballa-o-k.f@ezweb,ne,jp', subject: 'テストメール')
+    mail(from: 'from@example.com', to: 'shamballa-o-k.f@ezweb.ne.jp', subject: 'テストメール')
   end
 end
