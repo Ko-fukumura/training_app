f348ced6beb1b7ee0652edd41f220edf4d4c8b64
福村晃 <fukumurako@fukumuraakiranoMacBook-puro.local>
2020-02-12 03:43:44 +0900

    本番環境のデータベースを変更master18

--- lib/mail/send_grid.rb
+++ lib/mail/send_grid.rb
@@ -4,14 +4,17 @@ class Mail::SendGrid
   end
 
   def deliver!(mail)
-    from = SendGrid::Email.new(email: mail.from.first)
-    to = SendGrid::Email.new(email: mail.to.first)
-    subject = mail.subject
-    content = SendGrid.Content.new(type: 'text/plain', value: mail.body.raw_source)
-    sg_mail = SendGrid::Mail.new(from, subject, to, content)
+    sg_mail = SendGrid::Mail.new
+    sg_mail.from = SendGrid::Email.new(email: mail.from.first)
+    sg_mail.subject = mail.subject
+    personalization = SendGrid::Personalization.new
+    personalization.add_to(SendGrid::Email.new(email: mail.to.first))
+    personalization.subject = mail.subject
+    sg_mail.add_content(SendGrid::Content.new(type: 'text/plain', value: mail.body.raw_source))
+    sg_mail.add_personalization(personalization)
 
     sg = SendGrid::API.new(api_key: @settings[:api_key])
-    response =sg.client.mail._('send').post(request_body: sg_mail.to_json)
+    response = sg.client.mail._('send').post(request_body: sg_mail.to_json)
     Rails.logger.info response.status_code
   end
 end
